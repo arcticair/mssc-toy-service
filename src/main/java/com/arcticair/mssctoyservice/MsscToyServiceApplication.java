e6359d7866e13c0c3fbaeacd9ac17ed82f2ce4a6
serzhi <servaszhi@gmail.com>
2020-10-02 18:22:01 +0300

    initial commit 2

---
+++ src/main/java/com/arcticair/mssctoyservice/MsscToyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arcticair.mssctoyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscToyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscToyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcticair/mssctoyservice/MsscToyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcticair.mssctoyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscToyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
